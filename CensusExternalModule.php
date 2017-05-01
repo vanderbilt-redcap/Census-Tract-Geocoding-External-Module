b90913ad8a634fb624cad4110f753338415ee42d
Scott Pearson <scott.j.pearson@vanderbilt.edu>
2017-05-01 11:17:20 -0500

    v1.1

--- CensusExternalModule.php
+++ CensusExternalModule.php
@@ -3,10 +3,17 @@ require_once dirname(__FILE__) . '/../../external_modules/classes/ExternalModule
 
 class CensusExternalModule extends AbstractExternalModule
 {
-        function hook_save_record($project_id, $record, $instrument, $event_id, $group_id, $survey_hash, $response_id) {
+        function hook_survey_page($project_id, $record, $instrument, $event_id, $group_id, $survey_hash, $response_id) {
+		$this->addScript($project_id, $record, $instrument, $event_id, $group_id, $survey_hash, $response_id);
+	}
+
+        function hook_data_entry_form($project_id, $record, $instrument, $event_id, $group_id, $survey_hash, $response_id) {
+		$this->addScript($project_id, $record, $instrument, $event_id, $group_id);
+	}
+
+        function addScript($project_id, $record, $instrument, $event_id, $group_id, $survey_hash = null, $response_id = null) {
               	$module_data = ExternalModules::getProjectSettingsAsArray(array("vanderbilt_census_geocoder"), $project_id);
 		if ($project_id && $record && ($instrument == $module_data["instrument"]['value'])) {
-			\REDCap::allowProjects(array($project_id));
 			$addressField = $module_data['address']['value'];
 			$keys = $module_data['keys']['value'];
 			if (!is_array($keys)) {
@@ -18,82 +25,35 @@ class CensusExternalModule extends AbstractExternalModule
 			}
 
 			if (count($keys) == count($fields)) {
-				$rcData = \REDCap::getData($project_id, "array", array($record), array($addressField), array($event_id));
-				$address = $rcData[$record][$event_id][$addressField];
-				$encodedAddress = preg_replace("/\s+/", "+", $address);
-				$encodedAddress = preg_replace("/United States/", "+", $encodedAddress);
+				echo "<script>
+					function downloadCensusData() {
+						var address = $('#".$addressField."').val();
+						var keys = ".json_encode($keys).";
+						var fields = ".json_encode($fields).";
 
-				$url = "https://geocoding.geo.census.gov/geocoder/geographies/onelineaddress?address=".$encodedAddress."&benchmark=Public_AR_Census2010&vintage=Census2010_Census2010&layers=14&format=json";
-				# city, state, zip encoding
-				// $url = "https://geocoding.geo.census.gov/geocoder/geographies/address?street=&city=&state=&zip=&benchmark=Public_AR_Census2010&vintage=Census2010_Census2010&layers=14&format=json";
-				$ch = curl_init();
-				curl_setopt($ch, CURLOPT_URL, $url);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-				curl_setopt($ch, CURLOPT_VERBOSE, 0);
-				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-				curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-				curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
-				curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
-				$output = curl_exec($ch);
-				curl_close($ch);
+						if (address) {
+							var encodedAddress = address.replace(/\s+/, '+');
+							encodedAddress = encodedAddress.replace(/United States/, '+');
+							var url = 'https://geocoding.geo.census.gov/geocoder/geographies/onelineaddress?address='+encodedAddress+'&benchmark=Public_AR_Census2010&vintage=Census2010_Census2010&layers=14&format=json';
+							$.post(url, {}, function(json) {
+								var data = JSON.parse(json);
 
-				$json = json_decode($output, true);
-				# make into associative array for ease of concept; not necessary
-				$toLookup = array();
-				$i = 0;
-				foreach($keys as $key) {
-					$toLookup[$key] = $fields[$i];
-					$i++;
-				}
-
-				$firstField = \REDCap::getRecordIdField();
-				$uploadJson = array(array($firstField => $record));
-				$i = 0;
-				foreach ($toLookup as $key => $field) {
-					$uploadJson[0][$field] = "";
-					$i++;
-				}
-				if ($i > 0) {
-					# overwrite with blanks
-					$response = \REDCap::saveData('json', json_encode($uploadJson), 'overwrite');
-					if (count($response['errors']) > 0) {
-						foreach ($response['errors'] as $error) {
-							echo "<script>alert('".addslashes($error)."');</script>";
-						}
-						die("Failed at first upload:<br>".implode("<br>", $response['errors']));
-					}
-				}
-
-				if (isset($json['result']['addressMatches'][0]['geographies'])) {
-					$lookupTable = $json['result']['addressMatches'][0]["geographies"]["Census Blocks"][0];
-
-					# formulate JSON
-					$uploadJson = array(array());
-					$varsToUpload = 0;
-					foreach ($toLookup as $key => $field) {
-						if (isset($lookupTable[$key])) {
-							if ($field) {
-								$value = $lookupTable[$key];
-								$uploadJson[0][$field] = $value;
-								$varsToUpload++;
-							}
+								if (data && data['result'] && data['result']['addressMatches'] && data['result']['addressMatches'][0] && data['result']['addressMatches'][0]['geographies'] && data['result']['addressMatches'][0]['geographies']['Census Blocks']) {
+									var lookupTable = data['result']['addressMatches'][0]['geographies']['Census Blocks'][0];
+									for (var i=0; i < fields.length; i++) {
+										if (lookupTable[keys[i]]) {
+											$('#'+fields[i]).val(lookupTable[keys[i]]);
+										} else {
+											$('#'+fields[i]).val('');
+										}
+									}
+								});
 						}
 					}
-
-					# if have data to upload
-					if ($varsToUpload > 0) {
-						$uploadJson[0][$firstField] = $record;
-						# overwrite with blanks
-						$response = \REDCap::saveData('json', json_encode($uploadJson), 'overwrite');
-						if (count($response['errors']) > 0) {
-							foreach ($response['errors'] as $error) {
-								echo "<script>alert('".addslashes($error)."');</script>";
-							}
-							die("Failed at second upload:<br>".implode("<br>", $response['errors']));
-						}
-					}
-				}
+					$('#'".$addressField.").blur(function() {
+						downloadCensusData();
+					});
+				echo </script>";
 			}
 		}
 	}
