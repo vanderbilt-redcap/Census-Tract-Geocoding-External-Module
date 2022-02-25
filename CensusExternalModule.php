6ff27aaa24a6887d3586e68aa146b083edbb947a
Mark McEver <mark@fastmail.net>
2022-02-25 14:05:27 -0600

    Added support for multiple census years

--- CensusExternalModule.php
+++ CensusExternalModule.php
@@ -13,28 +13,71 @@ class CensusExternalModule extends AbstractExternalModule
 		$this->addScript($project_id, $record, $instrument, $event_id, $group_id);
 	}
 
-		function addScript($project_id, $record, $instrument, $event_id, $group_id, $survey_hash = null, $response_id = null) {
-			$module_data = ExternalModules::getProjectSettingsAsArray([$this->PREFIX], $project_id);
-		if ($project_id) {
-			$addressField = $module_data['address']['value'];
-			$latitudeField = $module_data['latitude']['value'];
-			$longitudeField = $module_data['longitude']['value'];
-			$keys = $module_data['keys']['value'];
-			if (!is_array($keys)) {
-				$keys = array($keys);
-			}
-			$fields = $module_data['fields']['value'];
-			if (!is_array($fields)) {
-				$fields = array($fields);
-			}
+	function redcap_every_page_before_render(){
+		$expectedUrl = APP_URL_EXTMOD . 'manager/ajax/get-settings.php';
+		$actualUrl = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'];
+		if($expectedUrl !== $actualUrl){
+			return;
+		}
 
-			if (count($keys) == count($fields)) {
+		$pid = $_POST['pid'] ?? null;
+		if($pid === null){
+			// We're on the system settings
+			return;
+		}
+		else{
+			$_GET['pid'] = $pid;
+
+			// Make sure old settings get transitioned
+			$this->getCensuses();
+
+			unset($_GET['pid']);
+		}
+	}
+
+	function transitionOldSettings(){
+		$keys = $this->getProjectSetting('keys');
+		$fields = $this->getProjectSetting('fields');
+
+		$this->setProjectSetting('year', ['2020']);
+		$this->setProjectSetting('censuses', ['true']);
+
+		$mappingValues = [];
+		foreach($fields as $field){
+			$mappingValues[] = "true";
+		}
+
+		$this->setProjectSetting('mappings', [$mappingValues]);
+
+		$this->setProjectSetting('keys', [$keys]);
+		$this->setProjectSetting('fields', [$fields]);
+	}
+
+	function getCensuses(){
+		$censuses = $this->getSubSettings('censuses');
+		if(!isset($censuses[0]['year'])){
+			$this->transitionOldSettings();
+			$censuses = $this->getSubSettings('censuses');
+		}
+
+		return $censuses;
+	}
+
+	function addScript($project_id, $record, $instrument, $event_id, $group_id, $survey_hash = null, $response_id = null) {
+		if ($project_id) {
+			$addressField = $this->getProjectSetting('address');
+			$latitudeField = $this->getProjectSetting('latitude');
+			$longitudeField = $this->getProjectSetting('longitude');
+
+			$censuses = $this->getCensuses();
+
+			foreach($censuses as $census){
 				echo '<script src="https://cdn.jsdelivr.net/npm/gasparesganga-jquery-loading-overlay@2.1.0/dist/loadingoverlay.min.js" integrity="sha384-MySkuCDi7dqpbJ9gSTKmmDIdrzNbnjT6QZ5cAgqdf1PeAYvSUde3uP8MGnBzuhUx"
 				crossorigin="anonymous"></script>';
 				echo "<script>
 				$(document).ready(function() {
 					console.log('Census Geocoder loaded');
-					var sharedArgs = 'benchmark=Public_AR_Current&vintage=Current_Current&format=json';
+					var sharedArgs = 'benchmark=Public_AR_Current&vintage=Census{$census['year']}_Current&format=json';
 
 					function downloadCensusData() {
 						var address = $('[name=\"".$addressField."\"]').val();
@@ -86,17 +129,17 @@ class CensusExternalModule extends AbstractExternalModule
 					}
 
 					function processCensusData(lookupTable) {
-						var keys = ".json_encode($keys).";
-						var fields = ".json_encode($fields).";
-
-						for (var i=0; i < fields.length; i++) {
-							var value = lookupTable[keys[i]]
+						const mappings = " . json_encode($census['mappings']) . "
+						
+						mappings.forEach(mapping => {
+							let value = lookupTable[mapping.keys]
 							if (!value) {
 								value = '';
 							}
 							
-							console.log('Setting '+fields[i]+' to '+value);
-							var field = $('[name=\"'+fields[i]+'\"]');
+							const fieldName = mapping.fields
+							console.log('Setting '+fieldName+' to '+value);
+							var field = $('[name=\"'+fieldName+'\"]');
 							field.val(value);
 							field.change();
 
@@ -105,7 +148,7 @@ class CensusExternalModule extends AbstractExternalModule
 								autocompleteField.val(field.find('option:selected').text())
 								autocompleteField.change()
 							}
-						}
+						})
 					}
 
 					// The following used to occur on the 'blur' event, but we switched it to 'change' since some
