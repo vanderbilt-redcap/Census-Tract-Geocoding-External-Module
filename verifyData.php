7e61307912b1c2fe7f22c132a97632eaf2fb9cdd
Mark McEver <mark@fastmail.net>
2022-03-02 14:27:15 -0600

    Added a verifyData page

---
+++ verifyData.php
@@ -0,0 +1,73 @@
+<?php
+
+if(SUPER_USER !== '1'){
+    die("You're not allowed to view this page!");
+}
+
+echo '<pre>';
+
+$recordIdFieldName = $module->getRecordIdField();
+$addressFieldName = $module->getProjectSetting('address');
+$fields = [
+    $recordIdFieldName,
+    $addressFieldName
+];
+
+$censuses = $module->getSubSettings('censuses');
+foreach($censuses as $census){
+    foreach($census['mappings'] as $mapping){
+        $fields[] = $mapping['fields'];
+    }
+}
+
+$records = json_decode(REDCap::getData([
+    'return_format' => 'json',
+    'fields' => $fields
+]), true);
+
+$dataToSave = [];
+foreach($censuses as $census){
+    foreach($records as $record){
+        $recordId = $record[$recordIdFieldName];
+        $module->log('checking record ' . $recordId);
+        $address = $record[$addressFieldName];
+        $address = str_replace(' ', '+', $address);
+        $_POST['get'] = "address=$address&benchmark=Public_AR_Current&vintage=Census{$census['year']}_Current&format=json";
+
+        ob_start();
+        require __DIR__ . '/getAddress.php';
+        $lookupTable = json_decode(ob_get_clean(), true)['result']['addressMatches'][0]['geographies']['Census Tracts'][0];
+
+        foreach($census['mappings'] as $mapping){
+            $key = $mapping['keys'];
+            $field = $mapping['fields'];
+
+            $expected = $lookupTable[$key];
+            $actual = $record[$field];
+
+            if($expected != $actual){
+                $message = "Record $recordId - Field $field - Expected '$expected', but found '$actual'";
+                $module->log($message);
+                echo "$message\n";
+
+                if(isset($_GET['save'])){
+                    $dataToSave[$recordId][$recordIdFieldName] = $recordId;
+                    $dataToSave[$recordId][$field] = $expected;
+                }
+            }
+        }
+    }
+}
+
+$dataToSave = array_values($dataToSave);
+
+if(!empty($dataToSave)){
+    $result = REDCap::saveData([
+        'dataFormat' => 'json',
+        'data' => json_encode($dataToSave)
+    ]);
+
+    var_dump($result);
+}
+
+echo '</pre>';
