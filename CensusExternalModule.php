be23545179df300f554c7ed2040da76ab49a2485
Kyle Chesney <kyle.2493@gmail.com>
2024-10-25 16:10:50 -0700

    Move frontend processing to separate file stop redifining JS functions for each census spec

--- CensusExternalModule.php
+++ CensusExternalModule.php
@@ -94,117 +94,31 @@ class CensusExternalModule extends AbstractExternalModule
 	}
 
 	function addScript($project_id, $record, $instrument, $event_id, $group_id, $survey_hash = null, $response_id = null) {
-		if ($project_id) {
-			$addressField = $this->getProjectSetting('address');
-			$latitudeField = $this->getProjectSetting('latitude');
-			$longitudeField = $this->getProjectSetting('longitude');
+		if (!$project_id) { return; }
+		$this->initializeJavascriptModuleObject();
 
-			$censuses = $this->getCensuses();
+		echo "<script> const module = " . $this->getJavascriptModuleObjectName() . "</script>";
 
-			foreach($censuses as $census){
-				echo '<script src="https://cdn.jsdelivr.net/npm/gasparesganga-jquery-loading-overlay@2.1.0/dist/loadingoverlay.min.js" integrity="sha384-MySkuCDi7dqpbJ9gSTKmmDIdrzNbnjT6QZ5cAgqdf1PeAYvSUde3uP8MGnBzuhUx"
+		$censuses = $this->getCensuses();
+
+		$this->tt_addToJavascriptModuleObject("censuses", $censuses);
+
+		$fields = [
+			"addressField" => $this->getProjectSetting('address'),
+			"latitudeField" => $this->getProjectSetting('latitude'),
+			"longitudeField" => $this->getProjectSetting('longitude')
+		];
+		$this->tt_addToJavascriptModuleObject("fields", $fields);
+
+		$urls = [
+			"getAddressUrl" => $this->getUrl("getAddress.php"),
+			"getCoordinatesUrl" => $this->getUrl("getCoordinates.php")
+		];
+		$this->tt_addToJavascriptModuleObject("urls", $urls);
+
+		echo '<script src="https://cdn.jsdelivr.net/npm/gasparesganga-jquery-loading-overlay@2.1.0/dist/loadingoverlay.min.js" integrity="sha384-MySkuCDi7dqpbJ9gSTKmmDIdrzNbnjT6QZ5cAgqdf1PeAYvSUde3uP8MGnBzuhUx"
 				crossorigin="anonymous"></script>';
-				echo "<script>
-				$(document).ready(function() {
-					console.log('Census Geocoder loaded');
-					var year = " . (int)$census['year'] . ";
 
-					function downloadCensusData() {
-						var address = $('[name=\"".$addressField."\"]').val();
-
-						if (address) {
-							var encodedAddress = address.replace(/United States/g, '');
-							console.log('Looking up '+encodedAddress);
-							$.post('".$this->getUrl('getAddress.php')."', { 'get':1,'address':encodedAddress, 'year': year}, function(json) {
-								console.log('Got data from TigerWeb');
-								console.log(json);
-								var data = JSON.parse(json);
-								if (data && data['result'] && data['result']['addressMatches'] && data['result']['addressMatches'][0] && data['result']['addressMatches'][0]['geographies'] && data['result']['addressMatches'][0]['geographies']['Census Blocks']) {
-									console.log('TigerWeb lookup data present');
-									var lookupTable = data['result']['addressMatches'][0]['geographies']['Census Blocks'][0];
-									processCensusData(lookupTable);
-								}
-							});
-						}
-					}
-
-					function downloadCensusDataFromLatLong() {
-						console.log('downloadCensusDataFromLatLong()')
-						
-						var latitude = $('[name=\"".$latitudeField."\"]').val();
-						var longitude = $('[name=\"".$longitudeField."\"]').val();
-
-						if(latitude && longitude) {
-							console.log('Looking up '+latitude+'/'+longitude);
-							$.LoadingOverlay('show')
-							$.ajax(
-							{
-								url:'".$this->getUrl('getCoordinates.php')."',
-								data:{
-									get: 1,
-									lat: latitude,
-									long: longitude,
-									year: year
-								},
-								type: 'POST'
-							}).done(function(json) {
-								$.LoadingOverlay('hide')
-								console.log('Got coordinate data');
-								console.log(json);
-								var data = JSON.parse(json);
-								if(data && data['result'] && data['result']['geographies'] && data['result']['geographies']['Census Blocks']) {
-									var lookupTable = data['result']['geographies']['Census Blocks'][0];
-									processCensusData(lookupTable);
-								}
-							});
-						}
-					}
-
-					function processCensusData(lookupTable) {
-						const mappings = " . json_encode($census['mappings']) . "
-						
-						mappings.forEach(mapping => {
-							let value = lookupTable[mapping.keys]
-							if (!value) {
-								value = '';
-							}
-							
-							const fieldName = mapping.fields
-							console.log('Setting '+fieldName+' to '+value);
-							var field = $('[name=\"'+fieldName+'\"]');
-							field.val(value);
-							field.change();
-
-							if(field.hasClass('rc-autocomplete')){
-								var autocompleteField = field.closest('td').find('.ui-autocomplete-input')
-								autocompleteField.val(field.find('option:selected').text())
-								autocompleteField.change()
-							}
-						})
-					}
-
-					// The following used to occur on the 'blur' event, but we switched it to 'change' since some
-					// modules update the field AFTER it has lost focus (like Address Autocompletion).
-					if('$addressField'){
-						$('[name=\"".$addressField."\"]').change(function() {
-							console.log('Looking up Census data');
-							downloadCensusData();
-						});
-					}
-
-					if('$latitudeField' && '$longitudeField'){
-						$('[name=\"".$latitudeField."\"]').change(function() {
-							downloadCensusDataFromLatLong();
-						});
-
-						$('[name=\"".$longitudeField."\"]').change(function() {
-							downloadCensusDataFromLatLong();
-						});
-					}
-				});
-				</script>";
-			}
-		}
+		echo "<script src='" . $this->getUrl("js/main.js") . "'></script>";
 	}
 }
-
